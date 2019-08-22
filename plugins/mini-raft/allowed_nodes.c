095897f0f36209b000828e3c01b1a090665f9235
Bernardo Ramos <bernardo@blocko.io>
2019-08-22 15:24:35 +0900

    separate allowed_nodes code

---
+++ plugins/mini-raft/allowed_nodes.c
@@ -0,0 +1,362 @@
+
+// ATTENTION: the first section of this file is temporary and the code will be
+//            soon removed or modified
+
+/****************************************************************************/
+/*** ALLOWED NODES **********************************************************/
+/****************************************************************************/
+
+#if 0
+/*
+** Check if the node is allowed to participate in the network
+*/
+SQLITE_PRIVATE int check_if_allowed_node(plugin *plugin, int id, int *pis_allowed) {
+  aergolite *this_node = plugin->this_node;
+  int count, rc;
+
+  SYNCTRACE("check_if_allowed_node id=%d\n", id);
+
+//! should it check the public key?
+//! should it use some authentication?
+
+// this function could be in the core
+
+  /* check if already in the list of known nodes */
+  rc = aergolite_consensus_db_query_int32(this_node, pis_allowed,
+         "SELECT count(*) FROM aergolite_allowed_nodes WHERE id=%d", id);
+  if( rc ) return;
+
+  return rc;
+}
+#endif
+
+/****************************************************************************/
+
+/*
+** Notes:
+** This function is reading from the main_db2.
+** It is currently NOT informing other nodes about the total_known_nodes.
+*/
+SQLITE_PRIVATE void update_known_nodes(plugin *plugin) {
+  aergolite *this_node = plugin->this_node;
+  node *node;
+
+  SYNCTRACE("update_known_nodes\n");
+
+  /* check if nodes already exist in the list of known nodes */
+
+  plugin->total_known_nodes = 1; // this node
+
+  //add_known_node(plugin, plugin->node_id);
+
+  for( node=plugin->peers; node; node=node->next ){
+    //add_known_node(plugin, node->id);
+    plugin->total_known_nodes++;
+  }
+
+  /* the leader must know the number of total known nodes, including those that are off-line */
+//  aergolite_queue_db_query_int32(this_node, &plugin->total_known_nodes, "SELECT count(*) FROM aergolite_allowed_nodes");
+
+  SYNCTRACE("update_known_nodes total_known_nodes=%d\n", plugin->total_known_nodes);
+
+}
+
+/****************************************************************************/
+
+SQLITE_PRIVATE void on_new_accepted_node(node *node) {
+  plugin *plugin = node->plugin;
+
+  SYNCTRACE("on_new_accepted_node\n");
+
+  if( plugin->is_leader ){
+    update_known_nodes(plugin);
+  }
+
+}
+
+/****************************************************************************/
+/* PEER FUNCTIONS ***********************************************************/
+/****************************************************************************/
+
+SQLITE_PRIVATE void stop_id_conflict_timer(struct node_id_conflict *id_conflict) {
+
+  id_conflict->existing_node->id_conflict = NULL;
+  id_conflict->new_node->id_conflict = NULL;
+
+  uv_timer_stop(&id_conflict->timer);
+  uv_close2( (uv_handle_t*) &id_conflict->timer, worker_thread_on_close);
+
+}
+
+SQLITE_PRIVATE void id_conflict_timer_cb(uv_timer_t* handle) {
+  //aergolite *this_node = (aergolite *) handle->loop->data;
+  struct node_id_conflict *id_conflict = (struct node_id_conflict *) handle->data;
+  node *existing_node, *new_node;
+
+  // 5. if the timer is fired before the answer:
+  //        1. close the old connection
+  //        2. continue with the identification process with the new node
+
+  existing_node = id_conflict->existing_node;
+  new_node = id_conflict->new_node;
+
+  stop_id_conflict_timer(id_conflict);
+
+  disconnect_peer(existing_node);
+
+  /* start the db sync */
+  on_new_accepted_node(new_node);
+
+}
+
+SQLITE_PRIVATE void on_id_conflict_recvd(node *node, void *msg, int size) {
+
+  sqlite3_log(1, "this node has the same node id as another one");
+
+}
+
+SQLITE_PRIVATE void on_id_conflict_sent(send_message_t *req, int status) {
+
+  /* disconnect the peer */
+  uv_close2( (uv_handle_t*) ((uv_write_t*)req)->handle, worker_thread_on_close);
+
+}
+
+SQLITE_PRIVATE void on_ping_received(node *node, void *msg, int size) {
+  binn *map = binn_map();
+  if( binn_map_set_int32(map, PLUGIN_CMD, PLUGIN_CMD_PONG) == FALSE ||
+      send_peer_message(node, map, NULL) == FALSE )
+  {
+    sqlite3_log(1, "on_ping_received: send_peer_message failed");
+  }
+  if (map) binn_free(map);
+}
+
+SQLITE_PRIVATE void on_ping_response(node *node, void *msg, int size) {
+  struct node_id_conflict *id_conflict;
+  binn *map;
+
+  id_conflict = node->id_conflict;
+  if( !id_conflict ) return;
+
+  /* send a message to the new node informing about the conflict */
+  map = binn_map();
+  if( binn_map_set_int32(map, PLUGIN_CMD, PLUGIN_ID_CONFLICT) == FALSE ||
+      send_peer_message(id_conflict->new_node, map, on_id_conflict_sent) == FALSE )
+  {
+    sqlite3_log(1, "on_ping_response: send_peer_message failed");
+  }
+  if (map) binn_free(map);
+
+  /* stop the timer */
+  stop_id_conflict_timer(id_conflict);
+
+}
+
+SQLITE_PRIVATE void on_new_node_with_same_id(node *existing_node, node *new_node) {
+  plugin *plugin = existing_node->plugin;
+  aergolite *this_node = existing_node->this_node;
+  struct node_id_conflict *id_conflict;
+  binn *map;
+
+  sqlite3_log(1, "new connected node has the same node id as another one");
+
+  // 1. send a packet to the already connected node
+  // 2. save info on a struct
+  // 3. start a timer and pass this struct
+  // 4. if the node answers before the timer:
+  //        1. stop the timer
+  //        2. send a message to the new connection informing about the colision
+  //        3. close the new connection when the packet is sent
+  // 5. if the timer is fired before the answer:
+  //        1. close the old connection
+  //        2. continue with the identification process with the new node
+
+  // or send the msg to all the nodes with this id?
+
+  //! this implementation is not perfect
+  //  if 2 new nodes connect with the same node_id, the behavior is undefined
+
+  /* send a packet to the already connected node */
+  map = binn_map();
+  if (binn_map_set_int32(map, PLUGIN_CMD, PLUGIN_CMD_PING) == FALSE) goto loc_failed1;
+  if (send_peer_message(existing_node, map, NULL) == FALSE) goto loc_failed1;
+  binn_free(map); map = 0;
+
+  /* save the conflict information */
+  id_conflict = sqlite3_malloc_zero(sizeof(struct node_id_conflict));
+  if( !id_conflict ) goto loc_failed2;
+  id_conflict->existing_node = existing_node;
+  id_conflict->new_node = new_node;
+
+  existing_node->id_conflict = id_conflict;
+  new_node->id_conflict = id_conflict;
+//--
+  //id_conflict->next = plugin->node_id_conflicts;
+  //plugin->node_id_conflicts = id_conflict;
+//++
+
+  /* initialize the node id conflict timer */
+  SYNCTRACE("starting the id conflict timer\n");
+  uv_timer_init(plugin->loop, &id_conflict->timer);
+  id_conflict->timer.data = id_conflict;  /* release on timer close */
+  uv_timer_start(&id_conflict->timer, id_conflict_timer_cb, 3000, 0);  /* wait 3 seconds */
+
+  return;
+loc_failed1:
+  sqlite3_log(1, "on_new_node_with_same_id: send_peer_message failed");
+  if (map) binn_free(map);
+loc_failed2:
+  disconnect_peer(existing_node);
+
+}
+
+SQLITE_PRIVATE void check_new_node_id(node *node) {
+  plugin *plugin = node->plugin;
+  aergolite *this_node = node->this_node;
+  struct node *tnode;
+
+  /* check for node id conflict with this node */
+  if (node->id == plugin->node_id) {
+    sqlite3_log(1, "new connected node has the same node id as this node");
+    goto loc_invalid_peer;
+  }
+
+  /* check for node id conflict with other node */
+  for (tnode = plugin->peers; tnode; tnode = tnode->next) {
+    if (tnode!=node && tnode->id == node->id) {
+      on_new_node_with_same_id(tnode, node);
+      return;
+    }
+  }
+
+  /* start the db sync */
+  on_new_accepted_node(node);
+
+  return;
+
+loc_invalid_peer:
+  /* disconnect the peer */
+  disconnect_peer(node);
+
+}
+
+/****************************************************************************/
+
+#if 0
+
+SQLITE_PRIVATE void on_new_id_request_sent(send_message_t *req, int status) {
+
+  if (status < 0) {
+    SYNCTRACE("on_new_id_request_sent FAILED - (%d) %s\n", status, uv_strerror(status));
+    uv_close2( (uv_handle_t*) ((uv_write_t*)req)->handle, worker_thread_on_close);  /* disconnect */
+  }
+
+}
+
+/****************************************************************************/
+
+SQLITE_PRIVATE void on_new_node_id_received(node *node, void *msg, int size) {
+  plugin *plugin = node->plugin;
+  aergolite *this_node = node->this_node;
+  int node_id;
+
+  node_id = binn_map_int32(msg, PLUGIN_NODE_ID);
+
+  if( node_id > 0 ){
+    /* save the node id in the local config */
+    int rc = aergolite_set_node_config_int(this_node, "node_id", node_id);
+    if( rc ){
+      sqlite3_log(rc, "on_new_node_id_received: could not save the new node id in the config");
+      /* disconnect the peer */
+      disconnect_peer(node);
+      return;
+    }
+    /* store it in the struct */
+    plugin->node_id = node_id;
+    /* check if valid and start db sync */
+    check_new_node_id(node);
+  }
+
+}
+
+/****************************************************************************/
+
+SQLITE_PRIVATE void on_new_node_id_request(node *node, void *msg, int size) {
+  plugin *plugin = node->plugin;
+  aergolite *this_node = node->this_node;
+  int  rc, node_id, max_node_id=0;
+  binn *map=0;
+
+  /* get a new node id */
+  node_id = aergolite_get_node_config_int(this_node, "last_node_id");
+  if( node_id > 1 ){
+    node_id++;
+  } else {
+    /* get an unused node id */
+    struct node *tnode;
+    for (tnode = plugin->peers; tnode; tnode = tnode->next) {
+      if (tnode->id > max_node_id) max_node_id = tnode->id;
+    }
+    if( max_node_id==0 ) max_node_id = 1;
+    node_id = max_node_id + 1;
+  }
+  if( node_id==plugin->node_id ) node_id++;
+  rc = aergolite_set_node_config_int(this_node, "last_node_id", node_id);
+  if( rc ){
+    sqlite3_log(rc, "on_new_node_id_request: could not save the new node id in the config");
+    goto loc_failed;
+  }
+
+  /* send it to the secondary node */
+  map = binn_map();
+  if (binn_map_set_int32(map, PLUGIN_CMD, PLUGIN_NEW_NODE_ID) == FALSE) goto loc_failed;
+  if (binn_map_set_int32(map, PLUGIN_NODE_ID, node_id) == FALSE) goto loc_failed;
+  if (send_peer_message(node, map, on_new_id_request_sent) == FALSE) {
+    sqlite3_log(1, "on_new_node_id_request: send_peer_message failed");
+loc_failed:
+    if (map) binn_free(map);
+    disconnect_peer(node);
+    return;
+  }
+
+  /* save the node id in the node struct */
+  node->id = node_id;
+
+  check_new_node_id(node);
+
+}
+
+#endif
+
+/****************************************************************************/
+
+SQLITE_PRIVATE void on_new_node_identified(node *node, void *msg, int size) {
+  plugin *plugin = node->plugin;
+  aergolite *this_node = node->this_node;
+
+  node->id = binn_map_int32(msg, PLUGIN_NODE_ID);
+
+  SYNCTRACE("remote node identified - node_id=%d\n", node->id);
+
+  /* if the node id was not supplied */
+  if( plugin->node_id==0 ){
+    sqlite3_log(1, "on_new_node_identified: empty node id!");
+    disconnect_peer(node);
+  } else if( node->id > 0 ){
+    /* xxx */
+    check_new_node_id(node);
+  }
+
+}
+
+/****************************************************************************/
+
+SQLITE_PRIVATE void on_id_msg_sent(send_message_t *req, int status) {
+
+  if (status < 0) {
+    SYNCTRACE("on_id_msg_sent FAILED - (%d) %s\n", status, uv_strerror(status));
+    uv_close2( (uv_handle_t*) ((uv_write_t*)req)->handle, worker_thread_on_close);  /* disconnect */
+  }
+
+}
