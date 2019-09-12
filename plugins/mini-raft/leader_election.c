a34975a6ac03b3de8694de67185e084eba1d59fa
Bernardo Ramos <bernardo@blocko.io>
2019-09-12 16:41:24 -0300

    separate leader query from leader election

--- plugins/mini-raft/leader_election.c
+++ plugins/mini-raft/leader_election.c
@@ -66,54 +66,71 @@ SQLITE_PRIVATE void start_leader_election(plugin *plugin) {
 
 SQLITE_PRIVATE void on_leader_check_timeout(uv_timer_t* handle) {
   plugin *plugin = (struct plugin *) handle->loop->data;
-  aergolite *this_node = plugin->this_node;
   struct leader_votes *votes;
   struct node *node;
-  int biggest=0, total=0;
+  int leader_id=0;
 
   SYNCTRACE("on_leader_check_timeout\n");
 
+  //if( plugin->total_known_nodes==0 ){  //! maybe this can be reactivated later when using the nodes table
+    update_known_nodes(plugin);
+  //}
+
   for( votes=plugin->leader_votes; votes; votes=votes->next ){
-    if( votes->count > biggest ) biggest = votes->count;
-    total += votes->count;
+    if( votes->count >= majority(plugin->total_known_nodes) ){
+      leader_id = votes->id;
+      break;
+    }
   }
 
-  if( total<2 ){
-    /* we need at least 3 nodes for a leader selection */
-    SYNCTRACE("on_leader_check_timeout: no sufficient votes (total=%d)\n", total);
-    goto loc_exit;
+  if( !plugin->in_election ){  /* inquirying the current leader */
+    if( leader_id==0 ){
+      SYNCTRACE("on_leader_check_timeout: no current leader\n");
+      start_leader_election(plugin);
+      goto loc_exit;
+    }else{
+      /* check if some node has a different leader */
+      for( votes=plugin->leader_votes; votes; votes=votes->next ){
+        if( votes->id!=leader_id && votes->id!=0 ){
+          SYNCTRACE("on_leader_check_timeout: some node(s) with a different leader\n");
+          /* start a leader election to make all nodes have the same leader */
+          start_leader_election(plugin);
+          goto loc_exit;
+        }
+      }
+    }
   }
 
-  /* is more than one with this amount of votes? */
-  total = 0;
-  for( votes=plugin->leader_votes; votes; votes=votes->next ){
-    if( votes->count == biggest ) total++;
-  }
-  if( total>1 ){
-    SYNCTRACE("on_leader_check_timeout: no consensus on the current leader\n");
+  if( plugin->in_election && leader_id==0 ){
+#ifdef DEBUGPRINT
+    int total = 0;
+    for( votes=plugin->leader_votes; votes; votes=votes->next ){
+      total += votes->count;
+    }
+    if( total<majority(plugin->total_known_nodes) ){
+      SYNCTRACE("on_leader_check_timeout: no sufficient votes (total=%d required=%d)\n",
+                total, majority(plugin->total_known_nodes));
+    }else{
+      SYNCTRACE("on_leader_check_timeout: no consensus on the current leader\n");
+    }
+#endif
     //start_leader_election(plugin);  -- it can start a new election within a timeout
     goto loc_exit;
   }
 
-  for( votes=plugin->leader_votes; votes; votes=votes->next ){
-    if( votes->count == biggest ) break;
-  }
 
-  if( votes->id==0 ){
-    /* no current leader. start a election on all nodes including this one */
-    SYNCTRACE("on_leader_check_timeout: no current leader\n");
-    if( !plugin->in_election ){
-      start_leader_election(plugin);
-    }
-    goto loc_exit;
-  }
-
-  for( node=plugin->peers; node; node=node->next ){
-    if( node->id==votes->id ){
-      plugin->leader_node = node;
-      break;
+  /* check who is the current leader */
+  if( leader_id==plugin->node_id ){
+    plugin->is_leader = TRUE;
+  }else{
+    for( node=plugin->peers; node; node=node->next ){
+      if( node->id==leader_id ){
+        plugin->leader_node = node;
+        break;
+      }
     }
   }
+
 
   if( plugin->leader_node ){
     SYNCTRACE("on_leader_check_timeout: the current leader is at %s:%d with id %d\n",
@@ -122,11 +139,10 @@ SQLITE_PRIVATE void on_leader_check_timeout(uv_timer_t* handle) {
 
     start_downstream_db_sync(plugin);
 
-  }else if( plugin->node_id==votes->id ){
+  }else if( plugin->is_leader ){
     SYNCTRACE("on_leader_check_timeout: this node is the current leader\n");
-    plugin->is_leader = TRUE;
 
-    update_known_nodes(plugin);
+    //update_known_nodes(plugin);
 
     //leader_node_process_local_transactions(plugin);
 
@@ -181,7 +197,6 @@ SQLITE_PRIVATE void check_current_leader(plugin *plugin) {
 ** not including the current leader
 */
 SQLITE_PRIVATE int calculate_new_leader(plugin *plugin){
-  aergolite *this_node = plugin->this_node;
   node *node;
   uint64 number, biggest=0, last_block, max_blocks=0;
 
@@ -227,7 +242,7 @@ SQLITE_PRIVATE int calculate_new_leader(plugin *plugin){
 
 /****************************************************************************/
 
-SQLITE_PRIVATE void broadcast_new_leader(plugin *plugin){
+SQLITE_PRIVATE void broadcast_leader_vote(plugin *plugin){
   char message[32];
   int leader_id;
 
@@ -235,9 +250,9 @@ SQLITE_PRIVATE void broadcast_new_leader(plugin *plugin){
 
   leader_id = calculate_new_leader(plugin);
 
-  SYNCTRACE("broadcast_new_leader id=%d\n", leader_id);
+  SYNCTRACE("broadcast_leader_vote id=%d\n", leader_id);
 
-  sprintf(message, "leader:%d", leader_id);
+  sprintf(message, "vote:%d", leader_id);
 
   send_tcp_broadcast(plugin, message);
 
@@ -247,9 +262,8 @@ SQLITE_PRIVATE void broadcast_new_leader(plugin *plugin){
 
 SQLITE_PRIVATE void election_info_timeout(uv_timer_t* handle) {
   plugin *plugin = (struct plugin *) handle->loop->data;
-  aergolite *this_node = plugin->this_node;
 
-  broadcast_new_leader(plugin);
+  broadcast_leader_vote(plugin);
 
 }
 
@@ -332,7 +346,7 @@ SQLITE_PRIVATE void on_leader_ping_response(
   if( plugin->leader_node==NULL && plugin->last_leader ){   //! what if the leader is not connected via TCP?
     char message[32];
     SYNCTRACE("the current leader answered\n");
-    sprintf(message, "leader:%d", plugin->last_leader->id);
+    sprintf(message, "vote:%d", plugin->last_leader->id);
     send_tcp_broadcast(plugin, message);
     uv_timer_stop(&plugin->election_info_timer);
   }
@@ -342,7 +356,7 @@ SQLITE_PRIVATE void on_leader_ping_response(
 /****************************************************************************/
 
 /* "last_block": a response message informing the height of the last block on the peer's blockchain */
-SQLITE_PRIVATE void on_requested_last_block(
+SQLITE_PRIVATE void on_peer_last_block(
   plugin *plugin,
   node *node,
   char *arg
@@ -375,7 +389,49 @@ SQLITE_PRIVATE void on_requested_peer_leader(
   struct leader_votes *votes;
   int node_id=0;
 
-  //check_peer_connection(plugin, sender, get_sockaddr_port(addr));
+  /* if an election was started in the middle of the inquiry, ignore the message */
+  if( plugin->in_election ) return;
+
+  node_id = atoi(arg);
+
+  for( votes=plugin->leader_votes; votes; votes=votes->next ){
+    if( votes->id==node_id ){
+      votes->count++;
+      break;
+    }
+  }
+
+  if( !votes ){  /* no item allocated for the given node id */
+    votes = sqlite3_malloc(sizeof(struct leader_votes));
+    if( !votes ) return;
+    votes->id = node_id;
+    votes->count = 1;
+    /* add it to the list */
+    votes->next = plugin->leader_votes;
+    plugin->leader_votes = votes;
+  }
+
+  /* stop the election timer if the number of votes for a single node reaches the majority */
+  if( plugin->total_known_nodes>1 && votes->count >= majority(plugin->total_known_nodes) ){
+    uv_timer_stop(&plugin->leader_check_timer);
+    on_leader_check_timeout(&plugin->leader_check_timer);
+  }
+
+}
+
+/****************************************************************************/
+
+/* "vote": a response message informing the leader vote */
+SQLITE_PRIVATE void on_leader_vote(
+  plugin *plugin,
+  node *node,
+  char *arg
+){
+  struct leader_votes *votes;
+  int node_id=0;
+
+  /* if this node is not in an election, ignore the message */
+  if( !plugin->in_election ) return;
 
   node_id = atoi(arg);
 
@@ -412,10 +468,13 @@ SQLITE_PRIVATE void leader_election_init(){
   register_tcp_message("leader?", on_get_current_leader);
   /* a response message informing the peer leader */
   register_tcp_message("leader", on_requested_peer_leader);
+
   /* a broadcast message requesting a leader election */
   register_tcp_message("election", on_new_election_request);
   /* a response message informing the height of the last block on the peer's blockchain */
-  register_tcp_message("last_block", on_requested_last_block);
+  register_tcp_message("last_block", on_peer_last_block);
+  /* a response message informing the leader vote */
+  register_tcp_message("vote", on_leader_vote);
   /* a response message from the current leader */
   register_tcp_message("pong", on_leader_ping_response);
 
